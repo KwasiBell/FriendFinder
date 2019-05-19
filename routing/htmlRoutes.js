4bb29f77791726c8999f20e5188f5a30e61f1858
KAB <47466350+KwasiBell@users.noreply.github.com>
2019-05-18 22:35:16 -0400

    updated package.json

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -2,10 +2,10 @@ ar path = require("path");
 
 module.exports = function(app) {
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname + "/../FriendFinder/public/survey.html"));
+    res.sendFile(path.join(__dirname + "../public/survey.html"));
   });
 
   app.use(function(req, res) {
-    res.sendFile(path.join(__dirname + "/../FriendFinder/public/home.html"));
+    res.sendFile(path.join(__dirname + "../public/survey.html"));
   });
 };
